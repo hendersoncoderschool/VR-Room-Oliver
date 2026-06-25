27ddabfe8596bbe28a07380fe266b75a77586f5a
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2026-06-25 12:52:44 -0700

    tv and flashlight

--- Assets/_Course Library/Scripts/Actions/PlayVideo.cs
+++ Assets/_Course Library/Scripts/Actions/PlayVideo.cs
@@ -56,6 +56,7 @@ public class PlayVideo : MonoBehaviour
     public void NextClip()
     {
         index = ++index % videoClips.Count;
+        videoPlayer.clip = videoClips[index];
         Play();
     }
 
@@ -70,6 +71,7 @@ public class PlayVideo : MonoBehaviour
         if (videoClips.Count > 0)
         {
             index = Random.Range(0, videoClips.Count);
+            videoPlayer.clip = videoClips[index];
             Play();
         }
     }
@@ -79,6 +81,7 @@ public class PlayVideo : MonoBehaviour
         if (videoClips.Count > 0)
         {
             index = Mathf.Clamp(value, 0, videoClips.Count);
+           
             Play();
         }
     }
